aa7c945092c4472047ae8489aa8706bdc3d4c1a7
Naan Mohammed <naanahmed.na@gmail.com>
2022-08-20 11:46:37 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Book_Shop__Web_II_Final_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
