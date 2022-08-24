a053477642f653497811dc392605c5e7f175e03a
Naan Mohammed <naanahmed.na@gmail.com>
2022-08-24 13:00:05 +0300

    - Add Category Controller, View -Edit _Layout.cshtml file to view Category instead of default privacy page

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Book_Shop.Data;
+using Book_Shop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Book_Shop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
