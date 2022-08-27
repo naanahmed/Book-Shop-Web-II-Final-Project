cc45d582eb1ffe195651c65a03acc2cb587c6935
Naan Mohammed <naanahmed.na@gmail.com>
2022-08-27 12:45:10 +0300

    Add model 'CoverType.cs' and connect to database & migrate

---
+++ Models/CoverType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Book_Shop.Models
+{
+    public class CoverType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Cover Type")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
