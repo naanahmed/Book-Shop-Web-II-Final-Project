03b17e53e904fc4e82bacd3b541a5d04db11cade
Muhammed <muhammedmurad1919@gmail.com>
2022-08-29 13:29:43 +0300

    Add Product model

---
+++ Areas/Admin/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Areas.Admin.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        
+        [Required]
+        public string ISBN { get; set; }
+        
+        [Required]
+        public string Author { get; set; }
+        
+        [Required]
+        [Range(1, 10000)]
+        public double ListPrice { get; set; }
+
+        public string ImageURL { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        
+        [Required]
+        public int CoverTypeId { get; set; }
+        [ForeignKey("CoverTypeId")]
+        public CoverType CoverType { get; set; }
+
+
+
+
+    }
+}
