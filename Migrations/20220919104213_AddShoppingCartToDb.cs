b0ca1427199ba751137ab9f7f898a34dc697a8a9
Naan Mohammed <naanahmed.na@gmail.com>
2022-09-19 13:44:16 +0300

    Add shoppig cart migrations

---
+++ Migrations/20220919104213_AddShoppingCartToDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookShopWebIIFinalProject.Migrations
+{
+    public partial class AddShoppingCartToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
