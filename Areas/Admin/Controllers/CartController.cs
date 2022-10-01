0cb20dfb0e4d50f481589f588dd65181c2ce5a1b
Naan Mohammed <naanahmed.na@gmail.com>
2022-10-01 19:26:07 +0300

    Add Shopping cart view

---
+++ Areas/Admin/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Shop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
